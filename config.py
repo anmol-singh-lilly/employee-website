9e07648e7a4fff4e1f1580ff4291ea486e313695
Anmol Kumar Singh <singh_anmol@lilly.com>
2023-01-05 17:20:55 +0530

    Basic Website Code Added

---
+++ config.py
@@ -0,0 +1,7 @@
+
+host = "employee-db.cv2vixmem2ag.us-east-1.rds.amazonaws.com" # endpoint of database
+user = "admin"
+passwd = "admin#123"
+db = "empdb"
+bucket = "emp-website-bucket"
+region = "us-east-1"
